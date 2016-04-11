ba3b7a9ed1c9b427445d7981cd7cbc4dc4a88e23
Johnathan Tran <johnathanphuctran21@gmail.com>
2016-04-11 16:57:34 -0700

    Set up basic template for MEAN stack webapp project

---
+++ app/routes.js
@@ -0,0 +1,89 @@
+var User = require('./models/user');
+var mongoose = require('mongoose');
+
+module.exports = function(app) {
+
+	// server routes ===========================================================
+	// handle things like api calls
+	// authentication routes
+    // sample api route
+    app.get('/api/users', function(req, res) {
+        // use mongoose to get all users in the database
+        User.find(function(err, users) {
+
+            // if there is an error retrieving, send the error.
+            // nothing after res.send(err) will execute
+            if (err)
+                res.send(err);
+
+            res.json(users); // return all users in JSON format
+        });
+    });
+
+    app.get('/api/users/:userId', function(req, res) {
+        // use mongoose to get all users in the database
+        User.findById(req.params.userId, function(err, users) {
+
+            // if there is an error retrieving, send the error.
+            // nothing after res.send(err) will execute
+            if (err)
+                res.send(err);
+
+            res.json(users); // return all users in JSON format
+        });
+    });
+
+    app.post('/api/users', function(req, res) {
+        // use mongoose to get all users in the database
+        var newId = mongoose.Types.ObjectId();
+        var newUser = {
+            _id: newId,
+            name: req.body.name
+        };
+        User.create(newUser, function(err, users) {
+
+            // if there is an error retrieving, send the error.
+            // nothing after res.send(err) will execute
+            if (err)
+                res.send(err);
+
+            res.json(users); // return all users in JSON format
+        });
+    });
+
+    app.put('/api/users/:userId', function(req, res) {
+        // use mongoose to get all users in the database
+        var updatedUser = {
+            name: req.body.name
+        };
+        User.findByIdAndUpdate(req.params.userId, updatedUser, function(err, users) {
+
+            // if there is an error retrieving, send the error.
+            // nothing after res.send(err) will execute
+            if (err)
+                res.send(err);
+
+            res.json(users); // return all users in JSON format
+        });
+    });
+
+    // sample api route
+    app.delete('/api/users/:userId', function(req, res) {
+        // use mongoose to get all users in the database
+        User.findByIdAndRemove(req.params.userId, function(err, users) {
+
+            // if there is an error retrieving, send the error.
+            // nothing after res.send(err) will execute
+            if (err)
+                res.send(err);
+
+            res.json(users); // return all users in JSON format
+        });
+    });
+	// frontend routes =========================================================
+	// route to handle all angular requests
+	app.get('*', function(req, res) {
+		res.sendfile('./public/index.html');
+	});
+
+};
---
+++ public/js/controllers/MainCtrl.js
@@ -0,0 +1,5 @@
+angular.module('MainCtrl', []).controller('MainController', function($scope) {
+
+	$scope.tagline = 'WHY ARENT MY AJAX CALLS WORKING';
+
+});
