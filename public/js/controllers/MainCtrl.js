da9131b9de44bd7e43e45841e7f7ae1e234140a3
Johnathan Tran <johnathanphuctran21@gmail.com>
2016-04-12 22:23:05 -0700

    Make basic ajax call for demo

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,5 +1,18 @@
-angular.module('MainCtrl', []).controller('MainController', function($scope) {
+angular.module('MainCtrl', []).controller('MainController', function($scope, $http) {
 
 	$scope.tagline = 'WHY ARENT MY AJAX CALLS WORKING';
 
+	$scope.changeTagline=function() {
+		var request = {
+			method: 'GET',
+			url: "http://localhost:3000/api/users/570dc21926f41e1c28ab4712"
+		};
+		$http(request).
+		success(function (data) {
+			$scope.tagline = data.name;
+		}).error(function (data) {
+			console.log('ERROR: Could not find user');
+		});
+
+	};
 });
