df74605082407e3612343c8deda8b60eb3845233
Johnathan Tran <johnathanphuctran21@gmail.com>
2016-04-16 00:56:19 -0700

    random question selected during ajax call

--- public/js/controllers/UserCtrl.js
+++ public/js/controllers/UserCtrl.js
@@ -1,63 +1,20 @@
 angular.module('UserCtrl', []).controller('UserController', function($scope, $http, $timeout) {
 
-	//totalApples is 4-20
-	var totalApples = Math.floor((Math.random() * 17) + 4);
-	//eatenApples is 0-totalApples
-	var eatenApples = Math.floor((Math.random() * totalApples+1));
-	//correct answer is:
-	var correctAnswer = totalApples - eatenApples;
-
-
-	// RETURNS PSEUDO-RANDOM NUMBER IN RANGE min...max
-	function random_number(min,max) {
-
-		return (Math.round((max-min) * Math.random() + min));
-	}
-
-	// CREATE AND FILL NUMBER ARRAY WITH UNIQUE RANDOM NUMBERS between 0-20 and one is the correct answer
-	var myNumArray = create_unique_random_array(4,0,totalApples);
-
-	function create_unique_random_array(num_elements,min,max) {
-
-		var temp, nums = new Array;
-
-		for (var element=0; element<num_elements; element++) {
-
-			//IMPORTANT: DON'T FORGET THE SEMI-COLON AT THE END
-			while((temp=number_found(random_number(min,max),nums))==-1);
-			nums[element] = temp;
-		}
-		if (nums.indexOf(correctAnswer) == -1) {
-			nums[Math.floor((Math.random()*4))] = correctAnswer;
-		}
-		return (nums);
-	}
-
-	function number_found (random_number,number_array) {
-
-		for (var element=0; element<number_array.length; element++) {
-
-			if (random_number==number_array[element]) {
-				return (-1);
-			}
-		}
-
-		return (random_number);
-	}
 
 	$request = {
 		method: 'GET',
-		url: "http://localhost:3000/api/questions/57103f05d10274e811ae55bd"
+		url: "http://localhost:3000/api/questions"
 	};
 
 	$http($request)
 		.success(function (data) {
-			$type = data.type;
-			$openingText = data.openingText;
-			$questionText = "I bought "+totalApples +" apples yesterday. If I eat "+eatenApples +" apples today, how many apples will I have left tomorrow?";
-			$correctAnswer = correctAnswer;
-			$response = data.response;
-			$choices = myNumArray;
+			randomQuestion = data[Math.floor((Math.random() * data.length) + 1) - 1];
+			$type = randomQuestion.type;
+			$openingText = randomQuestion.openingText;
+			$questionText = randomQuestion.questionText;
+			$correctAnswer = randomQuestion.correctAnswer;
+			$response = randomQuestion.response;
+			$choices = randomQuestion.choices;
 		})
 		.error(function (data) {
 			console.log('ERROR: Could not retrieve questions.');
@@ -94,18 +51,23 @@ angular.module('UserCtrl', []).controller('UserController', function($scope, $ht
 	};
 
 	$scope.mcResponse = function(answer) {
-		if (answer == myNumArray.indexOf(correctAnswer)) {
-			$scope.dialogue = "correct";
+		if (answer == 0) {
+			$scope.dialogue = $response[0];
+		} else if (answer == 1) {
+			$scope.dialogue = $response[1];
+		} else if (answer == 2) {
+			$scope.dialogue = $response[2];
+		} else if (answer == 3) {
+			$scope.dialogue = $response[3];
+		}
+		if (answer == $correctAnswer) {
 			$scope.showChoices = false;
 			$scope.showClose = true;
-		}
-		else{
-			$scope.dialogue = "wrong";
 		}
 	}
 
 	$scope.frResponse = function() {
-		$scope.dialogue = $scope.answer + "? That's my favorite color too!";
+		$scope.dialogue = $scope.frAnswer + "? That's my favorite color too!";
 		$scope.frAnswer = "";
 		$scope.showEntry = false;
 		$scope.showClose = true;
