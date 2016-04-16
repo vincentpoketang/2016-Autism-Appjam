0fba4afb55b1feadb5654a14253c0faf27cb0671
Bryce Tham <brycetham@gmail.com>
2016-04-16 16:35:00 -0700

    Added Timer, Random Questions
    Added timer and the ability to pull random questions from the database.
    Changed some CSS.

--- public/js/controllers/BryceCtrl.js
+++ public/js/controllers/BryceCtrl.js
@@ -1,27 +1,42 @@
 angular.module('BryceCtrl', []).controller('BryceController', function($scope, $http) {
-        
-    //Declare :)
-    $type = "";
-    $openingText = "";
-    $questionText = "";
-    $correctAnswer = "";
-    $response = "";
-    $choices = "";
+                  
+    $scope.clickable = true;
                                            
-    $getQuestion = function(id) {
+    $scope.countDown = 10;
+    setInterval(function(){
+        console.log("Countdown: " + $scope.countDown);
+        $scope.countDown--;
+        $scope.$apply();
+        if ($scope.countDown == 0 || $scope.showDialogue || $scope.clickable) {
+            if ($scope.countDown == 0) {
+                $scope.clickable = true;
+            }
+            $scope.countDown = 10;
+        }
+    }, 1000);
+                                        
+    $scope.toggleDialogue = function() {
+        if (!$scope.clickable) {
+            return;
+        } else {
+            $scope.clickable = false;
+        }
         $request = {
             method: 'GET',
-            url: "http://localhost:3000/api/questions/" + id
+            url: "http://localhost:3000/api/questions/"
         };
         $http($request)
         .success(function (data) {
-            $type = data.type;
-            $openingText = data.openingText;
-            $questionText = data.questionText;
-            $correctAnswer = data.correctAnswer;
-            $response = data.response;
-            $choices = data.choices;
-                 
+            // Get random question from database.
+            $index = Math.floor(Math.random()*data.length);
+            $type = data[$index].type;
+            $openingText = data[$index].openingText;
+            $questionText = data[$index].questionText;
+            $correctAnswer = data[$index].correctAnswer;
+            $response = data[$index].response;
+            $choices = data[$index].choices;
+
+            // Generate opening dialogue.
             $name = 'Bryce';
             $text = 'Hello ' + $name + '! ' + $openingText;
             $scope.showDialogue = true;
@@ -31,18 +46,12 @@ angular.module('BryceCtrl', []).controller('BryceController', function($scope, $
             $scope.dialogue = $text;
         })
         .error(function (data) {
-          console.log('Error: Could not retrieve question.');
+          console.log('Error: Could not connect to the database.');
         });
-
-    }
-                                           
-    $scope.toggleDialogue = function() {
-        $questions = ["5711330118b9db3d0329f7d1", "5711941d0600fef1035a7d9d", "571194d90600fef1035a7d9e"];
-        $id = $questions[Math.floor(Math.random()*$questions.length)];
-        $getQuestion($id);
     };
                                            
     $scope.toggleQuestion = function() {
+        // Generate random math problem.
         if ($type == "MA") {
             $varX = Math.floor((Math.random() * 17) + 4);
             $varY = Math.floor((Math.random() * $varX+1));
@@ -51,6 +60,7 @@ angular.module('BryceCtrl', []).controller('BryceController', function($scope, $
             $questionText = $questionText.replace("?_X", $varX);
             $questionText = $questionText.replace("?_Y", $varY);
         }
+        // Show choices and user input.
         if ($type == "MC" || $type == "MA") {
             $scope.choice1 = $choices[0];
             $scope.choice2 = $choices[1];
@@ -65,12 +75,13 @@ angular.module('BryceCtrl', []).controller('BryceController', function($scope, $
     };
                                            
     $scope.toggleClose = function() {
+        // Show close button.
         $scope.showDialogue = false;
         $scope.showChoices = false;
     };
                                            
     $scope.mcResponse = function(answer) {
-                                           console.log
+        // Generate responses for multiple choice.
         if ($type == "MA") {
             if (answer == $choices.indexOf($maSolution)) {
                 $res = $response[1].replace("?_X", $varX);
@@ -103,6 +114,7 @@ angular.module('BryceCtrl', []).controller('BryceController', function($scope, $
     }
                                            
     $scope.frResponse = function() {
+        // Generate responses for free response.
         $response = $response[1].replace("?_C", $scope.frAnswer.toLowerCase());
         $scope.dialogue = $response;
         $scope.frAnswer = "";
@@ -115,10 +127,12 @@ angular.module('BryceCtrl', []).controller('BryceController', function($scope, $
         $i = 0;
         while ($i < 3) {
             $newNumber = Math.floor(Math.random() * 20)
+            // If the number is not already a choice and not the solution, insert it.
             if ($numbers.indexOf($newNumber) <= -1 && $newNumber != $maSolution) {
                 $numbers[$i++] = $newNumber;
             }
         }
+        // Add solution to choices and shuffle array.
         $numbers[3] = $maSolution;
         $numbers.sort(function() { return 0.5 - Math.random() });
         return $numbers;
