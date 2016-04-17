896bcf0df980c84da0ffffc808abf1355b119d1c
kjyu1 <kevinyu0@yahoo.com>
2016-04-16 18:29:08 -0700

    User.js extra attributes

--- app/models/User.js
+++ app/models/User.js
@@ -4,5 +4,8 @@ var mongoose = require('mongoose');
 // define our user model
 // module.exports allows us to pass this to other files when it is called
 module.exports = mongoose.model('User', {
-	name : {type : String, default: ''}
+	name : {type : String, default: ''},
+	correctMathAnswers: Number,
+	favorites: [{String: String}]
+
 });
