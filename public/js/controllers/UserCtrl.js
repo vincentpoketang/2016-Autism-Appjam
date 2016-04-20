d70157fefbabee830e515a0b5f0378272cf62700
Johnathan Tran <johnathanphuctran21@gmail.com>
2016-04-19 22:08:18 -0700

    emotion starts at mad/sad after 5 questions he becomes happy

--- public/js/controllers/UserCtrl.js
+++ public/js/controllers/UserCtrl.js
@@ -1,39 +1,134 @@
 angular.module('UserCtrl', []).controller('UserController', function($scope, $http, $timeout) {
 
+	////////////////////////////////
+	// STARTING SCRIPT
+	///////////////////////////////
+	$scope.currentMood = Math.random() <= 0.5 ? "..\\assets\\Mad.png" : "..\\assets\\Sad.png";
+	console.log($scope.currentMood);
+
+	$moodCounter = 0;
 
 	$request = {
 		method: 'GET',
-		url: "http://localhost:3000/api/questions"
+		url: "http://localhost:3000/api/users/"
 	};
-
 	$http($request)
 		.success(function (data) {
-			randomQuestion = data[Math.floor((Math.random() * data.length) + 1) - 1];
-			$type = randomQuestion.type;
-			$openingText = randomQuestion.openingText;
-			$questionText = randomQuestion.questionText;
-			$correctAnswer = randomQuestion.correctAnswer;
-			$response = randomQuestion.response;
-			$choices = randomQuestion.choices;
+			$userID = data[0]._id;
+			$userName = data[0].name;
+			$userData = data[0].data;
 		})
 		.error(function (data) {
-			console.log('ERROR: Could not retrieve questions.');
+			console.log('Error: Could not retrieve user settings.');
 		});
 
-	$scope.toggleDialogue = function() {
-		$name = 'Bryce';
-		$text = 'Hello ' + $name + '! ' + $openingText;
+	// Start music.
+	$date = new Date();
+	$hours = $date.getHours();
+	if ($hours >= 5 && $hours <= 19) {
+		$audio = new Audio("../audio/BubbleBuddyDay.mp3");
+	} else {
+		$audio = new Audio("../audio/BubbleBuddyNight.mp3");
+	}
+	$audio.loop = true;
+	$audio.play();
 
-		$scope.showDialogue = true;
-		$scope.showNext = true;
-		$scope.showClose = false;
-		$scope.dialogue = $text;
+	$scope.clickable = true;
+
+	// Count down to next question/conversation.
+	$scope.countDown = 10;
+	setInterval(function(){
+		$scope.countDown--;
+		$scope.$apply();
+		// Pause countdown if dialogue opens.
+		if ($scope.countDown == 0 || $scope.showDialogue || $scope.clickable) {
+			if ($scope.countDown == 0) {
+				$scope.clickable = true;
+			}
+			$scope.countDown = 10;
+		}
+	}, 1000);
+
+
+	////////////////////////////////
+	// DIALOGUE METHODS
+	///////////////////////////////
+
+	$scope.toggleDialogue = function() {
+		if ($scope.clickable) {
+			$scope.clickable = false;
+			$retrieve = Math.floor(Math.random()*2);
+			$moodCounter++;
+			if($moodCounter > 5)
+				$scope.currentMood = "..\\assets\\happy.png";
+			if ($retrieve == 0) {
+				$getQuestion();
+			} else {
+				$getConversation();
+			}
+		}
 	};
 
+	$scope.toggleClose = function() {
+		// Show close button.
+		$scope.showDialogue = false;
+		$scope.showChoices = false;
+	};
+
+
+	////////////////////////////////
+	// QUESTION METHODS
+	///////////////////////////////
+
+	$getQuestion = function() {
+		$request = {
+			method: 'GET',
+			url: "http://localhost:3000/api/questions/"
+		};
+		$http($request)
+			.success(function (data) {
+				// Get random question from database.
+				$index = Math.floor(Math.random()*data.length);
+				$type = data[$index].type;
+				$openingText = data[$index].openingText;
+				$questionText = data[$index].questionText;
+				$correctAnswer = data[$index].correctAnswer;
+				$response = data[$index].response;
+				$choices = data[$index].choices;
+				$dataLoad = data[$index].dataLoad;
+
+				// Generate opening dialogue.
+				$name = 'Bryce';
+				$text = 'Hello ' + $name + '! ' + $openingText;
+				$scope.showDialogue = true;
+				$scope.showChoices = false;
+				$scope.showQuestion = true;
+				$scope.showClose = false;
+				$scope.dialogue = $text;
+			})
+			.error(function (data) {
+				console.log('Error: Could not retrieve questions.');
+			});
+	}
+
 	$scope.toggleQuestion = function() {
-		$scope.dialogue = $questionText;
-		$scope.showNext = false;
-		if ($type == "MC") {
+		// Generate random math problem.
+		if ($type == "MA-" || $type == "MA+") {
+			if ($type == "MA-") {
+				$varX = Math.floor((Math.random() * 17) + 4);
+				$varY = Math.floor((Math.random() * $varX+1));
+				$maSolution = $varX - $varY;
+			} else if ($type == "MA+") {
+				$varX = Math.floor((Math.random() * 10));
+				$varY = Math.floor((Math.random() * 10));
+				$maSolution = $varX + $varY;
+			}
+			$choices = $generateRandomArray(4, 0, $maSolution);
+			$questionText = $questionText.replace("?_X", $varX);
+			$questionText = $questionText.replace("?_Y", $varY);
+		}
+		// Show choices and user input.
+		if ($type == "MC" || $type == "MA-" || $type == "MA+") {
 			$scope.choice1 = $choices[0];
 			$scope.choice2 = $choices[1];
 			$scope.choice3 = $choices[2];
@@ -42,43 +137,147 @@ angular.module('UserCtrl', []).controller('UserController', function($scope, $ht
 		} else if ($type == "FR") {
 			$scope.showEntry = true;
 		}
-
+		$scope.dialogue = $questionText;
+		$scope.showQuestion = false;
 	};
 
-	$scope.toggleClose = function() {
-		$scope.showDialogue = false;
-		$scope.showChoices = false;
-	};
+
+	////////////////////////////////
+	// CONVERSATION METHODS
+	///////////////////////////////
+
+	$getConversation = function() {
+		$request = {
+			method: 'GET',
+			url: "http://localhost:3000/api/conversations/"
+		};
+		$http($request)
+			.success(function (data) {
+				// Get random conversation from database.
+				while (true) {
+					$index = Math.floor(Math.random()*data.length);
+					$type = data[$index].type;
+					$mood = data[$index].mood;
+					$time = data[$index].time;
+					$property = data[$index].property;
+					$conversation = data[$index].text;
+					if ($property == "None" || $userData.hasOwnProperty($property)) {
+						break;
+					}
+				}
+
+				$scope.showDialogue = true;
+				$scope.showChoices = false;
+				$scope.showConversation = true;
+				$scope.showClose = false;
+				$conversationIndex = 0;
+				$scope.dialogue = $conversation[$conversationIndex];
+			})
+			.error(function (data) {
+				console.log('Error: Could not retrieve conversations.');
+			});
+	}
+
+	$scope.toggleConversation = function() {
+		// Move on to next part of conversation.
+		if ($conversationIndex == $conversation.length-2) {
+			$scope.showConversation = false;
+			$scope.showClose = true;
+		}
+		// Iterate to next line.
+		$c = $conversation[++$conversationIndex];
+		// Check if there are any variables needed to be replaced.
+		if ($c.includes("?_")) {
+			$replace = $c.match(/\?_\w+/g);
+			$scope.dialogue = $c.replace($replace, $userData[$property]);
+		} else {
+			$scope.dialogue = $c;
+		}
+	}
+
+
+	////////////////////////////////
+	// RESPONSE METHODS
+	///////////////////////////////
 
 	$scope.mcResponse = function(answer) {
-		if (answer == 0) {
-			$scope.dialogue = $response[0];
-		} else if (answer == 1) {
-			$scope.dialogue = $response[1];
-		} else if (answer == 2) {
-			$scope.dialogue = $response[2];
-		} else if (answer == 3) {
-			$scope.dialogue = $response[3];
-		}
-		if (answer == $correctAnswer) {
-			$scope.showChoices = false;
-			$scope.showClose = true;
+		// Generate responses for multiple choice.
+		if ($type == "MA-" || $type == "MA+") {
+			if (answer == $choices.indexOf($maSolution)) {
+				$res = $response[1].replace("?_X", $varX);
+				$res = $res.replace("?_Y", $varY);
+				$res = $res.replace("?_C", $maSolution);
+				$scope.dialogue = $res;
+				$scope.showChoices = false;
+				$scope.showClose = true;
+				$load($dataLoad, $userData["correctMathAnswers"] + 1);
+			} else {
+				$res = $response[0].replace("?_X", $varX);
+				$res = $res.replace("?_Y", $varY);
+				$res = $res.replace("?_C", $choices[answer]);
+				$scope.dialogue = $res;
+			}
+		} else if ($type == "MC") {
+			if (answer == 0) {
+				$scope.dialogue = $response[0];
+			} else if (answer == 1) {
+				$scope.dialogue = $response[1];
+			} else if (answer == 2) {
+				$scope.dialogue = $response[2];
+			} else if (answer == 3) {
+				$scope.dialogue = $response[3];
+			}
+			if (answer == $correctAnswer) {
+				$scope.showChoices = false;
+				$scope.showClose = true;
+			}
 		}
 	}
 
 	$scope.frResponse = function() {
-		$scope.dialogue = $scope.frAnswer + "? That's my favorite color too!";
-		$scope.frAnswer = "";
+		// Generate responses for free response.
+		$response = $response[1].replace("?_C", $scope.frAnswer.toLowerCase());
+		$scope.dialogue = $response;
 		$scope.showEntry = false;
 		$scope.showClose = true;
+		$load($dataLoad, $scope.frAnswer);
 	}
 
-	$scope.countDown = 10;
-	var timer = setInterval(function(){
-		$scope.countDown--;
-		$scope.$apply();
-		if($scope.countDown == 0)
-			$scope.countDown = 10
-	}, 1000);
 
+	////////////////////////////////
+	// HELPER METHODS
+	///////////////////////////////
+
+	$load = function(property, value) {
+		$userData[property] = value;
+		$scope.frAnswer = "";
+		$request = {
+			method: 'PUT',
+			url: "http://localhost:3000/api/users/" + $userID,
+			data: {data: $userData}
+		};
+		$http($request)
+			.success(function (data) {
+				console.log('Updated user setting "' + property + '" to "' + value + '".');
+			})
+			.error(function (data) {
+				console.log('Error: Could not modify user settings.');
+			});
+	}
+
+	$generateRandomArray = function(n, min, max) {
+		$numbers = new Array;
+		$i = 0;
+		while ($i < 3) {
+			$newNumber = Math.floor(Math.random() * 20)
+			// If the number is not already a choice and not the solution, insert it.
+			if ($numbers.indexOf($newNumber) <= -1 && $newNumber != $maSolution) {
+				$numbers[$i++] = $newNumber;
+			}
+		}
+		// Add solution to choices and shuffle array.
+		$numbers[3] = $maSolution;
+		$numbers.sort(function() { return 0.5 - Math.random() });
+		return $numbers;
+	}
 });
