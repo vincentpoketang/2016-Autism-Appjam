48ad677aca1ba082f3363292478bbff565c18207
Johnathan Tran <johnathanphuctran21@gmail.com>
2016-04-20 23:06:06 -0700

    New routes id routes should work now

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,7 @@ var Question = require('./models/question');
 var Conversation = require('./models/conversation');
 var mongoose = require('mongoose');
 
+
 module.exports = function(app) {
 
 	// server routes ===========================================================
@@ -22,15 +23,14 @@ module.exports = function(app) {
         });
     });
 
-    app.get('/api/users/:userId', function(req, res) {
+    app.get('/api/users/:userid', function(req, res) {
         // use mongoose to get all users in the database
-        User.findById(req.params.userId, function(err, users) {
+        User.findOne(req.params.userid, function(err, users) {
 
             // if there is an error retrieving, send the error.
             // nothing after res.send(err) will execute
             if (err)
                 res.send(err);
-
             res.json(users); // return all users in JSON format
         });
     });
@@ -61,9 +61,9 @@ module.exports = function(app) {
         });
     });
 
-    app.put('/api/users/:userId', function(req, res) {
+    app.put('/api/users/:userid', function(req, res) {
         // use mongoose to get all users in the database
-        User.findByIdAndUpdate(req.params.userId, req.body, function(err, users) {
+        User.findOneAndUpdate(req.params.userid, req.body, function(err, users) {
 
             // if there is an error retrieving, send the error.
             // nothing after res.send(err) will execute
@@ -75,9 +75,9 @@ module.exports = function(app) {
     });
 
     // sample api route
-    app.delete('/api/users/:userId', function(req, res) {
+    app.delete('/api/users/:userid', function(req, res) {
         // use mongoose to get all users in the database
-        User.findByIdAndRemove(req.params.userId, function(err, users) {
+        User.findOneAndRemove(req.params.userid, function(err, users) {
 
             // if there is an error retrieving, send the error.
             // nothing after res.send(err) will execute
@@ -128,9 +128,9 @@ module.exports = function(app) {
         });
     });
 
-    app.get('/api/questions/:questionId', function(req, res) {
+    app.get('/api/questions/:questionid', function(req, res) {
         // use mongoose to get all users in the database
-        Question.findById(req.params.questionId, function(err, question) {
+        Question.findOne(req.params.userid, function(err, question) {
 
             // if there is an error retrieving, send the error.
             // nothing after res.send(err) will execute
